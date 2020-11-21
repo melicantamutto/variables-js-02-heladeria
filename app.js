6b48d9d8527226926dfd63181739de9536dd85f0
MELI CANTAMUTTO <MELICANTAMUTTO@gmail.com>
2020-11-21 12:48:24 -0300

    ejercicio js

---
+++ app.js
@@ -0,0 +1,9 @@
+console.log('Heladería');
+
+alert('Hola! Ingresá porfavor los gustos de helado que deseas');
+
+const gusto1 = prompt('Ingrese el primer sabor');
+const gusto2 = prompt('Ingrese el segundo sabor');
+const gusto3 = prompt('Ingrese el tercer sabor');
+
+confirm(`Confirmas que tus gustos elegidos son ${gusto1}, ${gusto2} y ${gusto3}?`);
